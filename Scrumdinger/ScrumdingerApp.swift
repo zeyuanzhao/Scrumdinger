ca3907107fb1ee5ee6dcc869d104143321e5f93f
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2025-03-27 23:27:16 -0400

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Alex zhao on 3/27/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
